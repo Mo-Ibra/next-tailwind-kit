50f572de415e9adfbf118a9ac1f05d7b938c37fe
Mo-Ibra <mohamedelgedwy245@gmail.com>
2025-01-05 23:06:44 +0200

    Added Sections

--- app/page.js
+++ app/page.js
@@ -1,12 +1,25 @@
 import Container from "@/components/layout/Container";
 import Navbar1 from "@/components/layout/Navbar";
+import Login from "@/components/sections/auth/login/Login";
+import Register from "@/components/sections/auth/register/Register";
+import Contact1 from "@/components/sections/contact/Contact1";
+import Faq1 from "@/components/sections/faq/Faq1";
+import Faq2 from "@/components/sections/faq/Faq2";
 import Features1 from "@/components/sections/features/Features1";
 import Features2 from "@/components/sections/features/Features2";
 import Features3 from "@/components/sections/features/Features3";
 import Features4 from "@/components/sections/features/Features4";
 import { Hero1, Hero2, Hero3, Hero4 } from "@/components/sections/hero";
+import Pricing1 from "@/components/sections/pricing/Pricing1";
 import Testimonial1 from "@/components/sections/testimonials/Testimonials";
 import Testimonials2 from "@/components/sections/testimonials/Testimonials2";
+import Contact2 from "@/components/sections/contact/Contact2";
+import Blog1 from "@/components/sections/blog/Blog1";
+import Careers1 from "@/components/sections/careers/Careers1";
+import Careers2 from "@/components/sections/careers/Careers2";
+import CTA1 from "@/components/sections/cta/Cta1";
+import Footer1 from "@/components/sections/footer/Footer1";
+import Footer2 from "@/components/sections/footer/Footer2";
 
 export default function Home() {
   return (
@@ -22,6 +35,19 @@ export default function Home() {
       <Features2 />
       <Features3 />
       <Features4 />
+      <Pricing1 />
+      <Register />
+      <Login />
+      <Faq1 />
+      <Faq2 />
+      <Contact1 />
+      <Contact2 />
+      <Blog1 />
+      <Careers1 />
+      <Careers2 />
+      <CTA1 />
+      <Footer1 />
+      <Footer2 />
     </Container>
   );
 }
---
+++ components/sections/auth/login/Login.jsx
@@ -0,0 +1,52 @@
+import { FcGoogle } from "react-icons/fc";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const Login = () => {
+  return (
+    <section className="py-32">
+      <div className="container">
+        <div className="flex flex-col gap-4">
+          <div className="mx-auto w-full max-w-sm rounded-md p-6 shadow">
+            <div className="mb-6 flex flex-col items-center">
+              <img
+                src="https://shadcnblocks.com/images/block/block-1.svg"
+                alt="logo"
+                className="mb-7 h-10 w-auto"
+              />
+              <p className="mb-2 text-2xl font-bold">Login to your account</p>
+            </div>
+            <div>
+              <div className="grid gap-4">
+                <Input type="email" placeholder="Enter your email" required />
+                <div>
+                  <Input
+                    type="password"
+                    placeholder="Enter your password"
+                    required
+                  />
+                </div>
+                <Button type="submit" className="mt-2 w-full">
+                  Login to your account
+                </Button>
+                <Button variant="outline" className="w-full">
+                  <FcGoogle className="mr-2 size-5" />
+                  Login with Google
+                </Button>
+              </div>
+              <div className="mx-auto mt-8 flex justify-center gap-1 text-sm text-muted-foreground">
+                <p>Don't have an account?</p>
+                <a href="#" className="font-medium text-primary">
+                  Register
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
